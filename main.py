578c93db87f82a967d2f73d9ff850b6b1a4a7935
James Hart <james.elliot.hart@gmail.com>
2020-06-08 18:52:51 -0500

    make headless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from contextlib import contextmanager
 import time
 
@@ -6,13 +7,15 @@ import time
 URL = 'https://www.homedepot.com/p/Husky-36-in-W-x-72-in-H-x-24-in-D-Steel-Garage-Gear-Cabinet-G3624W-US/206497845'
 
 @contextmanager
-def open_driver(options=webdriver.ChromeOptions()):
+def open_driver(options=Options()):
     driver = webdriver.Chrome(chrome_options=options)
     yield driver
     driver.quit()
 
 def check_availability(url):
-    with open_driver() as driver:
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    with open_driver(chrome_options) as driver:
         driver.get(URL)
         time.sleep(1)
         out_of_stock = driver.find_elements_by_class_name('oos-online')
